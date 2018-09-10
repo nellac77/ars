31d6087654e8eeb354cb3197c638000ef334292c
Cal Johnson <nellac77@pop-os.localdomain>
2018-09-09 21:45:39 -0500

    Add ars.py for the model implementation

---
+++ ars.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ars.py: An implementation of the Augmented Rrandom Search AI model in python.
+
+Created on Sun Sep  9 21:43:48 2018
+
+@author: nellac77
+"""
+
